b8aaec92cdf4591b00f42a01240336ba8784089b
Jyjystudio <jyhad.benyoussef@gmail.com>
2022-03-21 11:06:37 +0100

    feat : implementation de la factory FactoryCard

--- js/App.js
+++ js/App.js
@@ -1,33 +1,42 @@
 class App {
 	constructor() {
 		this.$moviesWrapper = document.querySelector(".movies-wrapper");
-		this.$modalWrapper = document.querySelector('.modal')
+		this.$modalWrapper = document.querySelector(".modal");
 
 		// this.oldMoviesApi = new MovieApi("data/old-movie-data.json");
 		this.newMoviesApi = new MovieApi("data/new-movie-data.json");
 		this.externalMoviesApi = new MovieApi("data/external-movie-data.json");
+		this.tvShowApi = new MovieApi("data/tv-show.json");
 	}
 
 	async main() {
 		// const oldMoviesData = await this.oldMoviesApi.getMovies();
 		const newMoviesData = await this.newMoviesApi.getMovies();
 		const externalMoviesData = await this.externalMoviesApi.getMovies();
+		const tvShowData = await this.tvShowApi.getMovies();
 
-		const ModalForm = new Form()
-        ModalForm.render()
+		const ModalForm = new Form();
+		ModalForm.render();
 
 		// const oldMovies = oldMoviesData.map(movie => new FactoryMovie("oldApi", movie));
-		const newMovies = newMoviesData.map(movie => new FactoryMovie("newApi", movie));
-		const externalMovies = externalMoviesData.map(movie => new FactoryMovie("externalApi", movie));
+		const newMovies = newMoviesData.map(
+			(movie) => new FactoryMovie("newApi", movie)
+		);
+		const externalMovies = externalMoviesData.map(
+			(movie) => new FactoryMovie("externalApi", movie)
+		);
+		const tvShow = tvShowData.map(
+			(movie) => new FactoryMovie("tv-show", movie)
+		);
 
-		const allMovies = newMovies.concat(externalMovies);
-		
+		const allMovies = newMovies.concat(externalMovies).concat(tvShow);
+
 		allMovies.forEach((movie) => {
-				// console.log(movie);
-				const Template = new MovieCard(movie);
-				console.log(Template);
-				this.$moviesWrapper.appendChild(Template.createMovieCard());
-			});
+			console.log(movie);
+			const Template = new FactoryCard(movie.type, movie);
+			console.log(Template);
+			this.$moviesWrapper.appendChild(Template.createCard());
+		});
 	}
 }
 
