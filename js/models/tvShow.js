9ac0b7de28fee7b8dfaf11e35a9d3391ca5281e0
Jyjystudio <jyhad.benyoussef@gmail.com>
2022-03-21 11:07:20 +0100

    feat : gestion des series

---
+++ js/models/tvShow.js
@@ -0,0 +1,37 @@
+// //constructor pattern
+class TvShow {
+	constructor(data) {
+		this._title = data.title;
+		this._picture = data.picture;
+		this._released_in = data.released_in;
+		this._duration = data.durationPerEpisode;
+		this._synopsis = data.synopsis;
+		this._type = data.type;
+		this._numberOfSeasons = data.numberOfSeasons;
+	}
+
+	get picture() {
+		return `/assets/thumbnails/${this._picture}`;
+	}
+	get thumbnail() {
+		return `/assets/thumbnails/${this._picture}`;
+	}
+	get title() {
+		return this._title;
+	}
+	get released_in() {
+		return this._released_in;
+	}
+	get duration() {
+		return `${this._duration} minutes`;
+	}
+	get synopsis() {
+		return this._synopsis;
+	}
+	get type() {
+		return this._type;
+	}
+	get numberOfSeasons() {
+		return this._numberOfSeasons;
+	}
+}
